4a594d4dbd9ffa7652b7e950cdfed5cba24964e7
luyx0 <arromm10@gmail.com>
2023-05-18 01:05:58 +0300

    TopBar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,31 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+  ],
+  overrides: [
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-  }
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/semi': 0,
+  },
 };
